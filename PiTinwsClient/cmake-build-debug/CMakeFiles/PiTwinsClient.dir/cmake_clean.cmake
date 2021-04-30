1436a946498949ae22f3ff769bd10c07424e5865
will.zhang <will.zhang@cloudminds.com>
2021-04-30 18:33:06 +0800

    add client

---
+++ PiTinwsClient/cmake-build-debug/CMakeFiles/PiTwinsClient.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PiTwinsClient.dir/main.cpp.obj"
+  "PiTwinsClient.exe"
+  "PiTwinsClient.exe.manifest"
+  "PiTwinsClient.lib"
+  "PiTwinsClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PiTwinsClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
