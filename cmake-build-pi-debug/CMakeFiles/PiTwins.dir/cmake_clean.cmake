2086079579256823f4408a6bcaca49daa5f79725
will.zhang <122812210@qq.com>
2021-05-01 10:07:50 +0800

    change folder struct

---
+++ cmake-build-pi-debug/CMakeFiles/PiTwins.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PiTwins.dir/PiTwins.cpp.o"
+  "CMakeFiles/PiTwins.dir/Private/StatusPublisher.cpp.o"
+  "CMakeFiles/PiTwins.dir/Private/VideoPublisher.cpp.o"
+  "PiTwins"
+  "PiTwins.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PiTwins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
